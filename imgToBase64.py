44315582dfaa5bb32ce6771618364ab14bd5a25c
SeleneSV <104835552+SeleneSV@users.noreply.github.com>
2022-09-08 13:13:32 -0500

    Delete imgToBase64.py

--- imgToBase64.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from io import BytesIO
-import base64
-
-with open(r'C:\Users\user\Desktop\collection\Emanuel\Emanuel.jpg', "rb") as f:
-    im_b64 = base64.b64encode(f.read())
-
-print(im_b64))
-
