fd344e0bdbb7bde4026fbdfbc553678ea004ead7
SeleneSV <ssolanov@unal.edu.co>
2022-09-15 14:25:03 -0500

    lambdaSNS: envía url del dashboard unknown

--- lambdaSNS.py
+++ lambdaSNS.py
@@ -5,34 +5,22 @@ Función para enviar SNS por correo cuando se actualiza un item de una tabla de 
 #Se importan librerías
 import json
 import boto3
+import io
 
 def lambda_handler(event, context):
 
-   #Se toma el primary key del item que se actualizó
-    Id = event['Records'][0]['dynamodb']['Keys']['FaceID']['S']
-   #Se toma el valor de la columna status del item que se actualizó
-    status=event['Records'][0]['dynamodb']['NewImage']['Status']['S']
+   client = boto3.client('sns')
 
-    print('identificación')
-    print(Id)
-    print('actualización status')
-    print(status)
+   snsArn = 'arn:aws:sns:us-east-1:533886999211:FaceSNS'
+   message = "Persona desconocida. \n http://localhost:3000/d/a2DTA1G4z/control?orgId=1&refresh=10s&from=1663229818365&to=1663251418365&viewPanel=12"
 
-   #Si el estatus es 'NO' se envía mensaje de "persona desconocida" a correo preestablecido 
-    if status == 'NO':
+   response = client.publish(
+      TopicArn = snsArn,
+      Message = message ,
+      Subject='Seguridad Telemetrik',
+   )
 
-      client = boto3.client('sns')
-
-      snsArn = 'arn:aws:sns:us-east-1:533886999211:FaceSNS'
-      message = "Persona desconocida."
-
-      response = client.publish(
-         TopicArn = snsArn,
-         Message = message ,
-         Subject='Seguridad Telemetrik'
-      )
-
-      return {
-         'statusCode': 200,
-         'body': json.dumps(response)
-      }
+   return {
+      'statusCode': 200,
+      'body': json.dumps(response)
+   }
