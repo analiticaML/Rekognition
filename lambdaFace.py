8033b678ab7dfede7a4a25d533d0c78c399e424e
SeleneSV <ssolanov@unal.edu.co>
2022-09-15 12:03:18 -0500

    Correción timeDifference y personas desconocidas

--- lambdaFace.py
+++ lambdaFace.py
@@ -20,9 +20,11 @@ import io
 from PIL import Image
 import datetime 
 import mysql.connector
-import math
-import time
 from datetime import datetime as dt
+import json
+import base64
+from PIL import ExifTags
+
 
 
 #Se define función principal de ejecución: Función Handler del evento de S3
@@ -76,8 +78,7 @@ def lambda_handler(event, context):
             updateItemDB(imgsid[0],str(date),str(time),conexion,similarity[0],confidence[0])
 
             #Se llama la función para obtener diferencia de tiempos entre capturas para una misma persona
-            delta = timeDifference(conexion,time,imgsid[0])
-            print(delta)
+            delta = timeDifference(conexion,time,imgsid[0],date)
 
             #Si la diferencia entre capturas es mayor a cinco segundo se crea un registro
             if delta > 5:
@@ -93,6 +94,42 @@ def lambda_handler(event, context):
 
             #Si se identificó a la persona en la colección se elimina la imagen del bucket
             deleteObject(bucket,key)
+
+
+#Función para conectar con una base de datos de mysql
+#Recibe como parámetros el usuario, la contraseña, el host, el nombre de la base de datos
+#y el puerto de la base de datos de mysql
+def mysql_start_connection(user, password, host, database, port):
+    try:
+        #Se establece la conexión ingresando los parámetros de la base de datos de mysql
+        conexion = mysql.connector.connect(user = user, password=password, host = host, database = database, port =port)
+        print("conexion exitosa")
+    except:
+        print("falla en la conexion ")
+    #Retorna la conexión establecida con la base de datos
+    return conexion
+
+
+
+#Función para desplegar la tabla en terminal
+#Recibe la conexión inicial con la base de datos de mysql
+def mysql_display(conexion):
+
+    #Se seleccionan todos los items de la tabla
+    cursor = conexion.cursor()
+    cursor.execute("Select * from control;")
+
+    #Se crea una lista con las filas de la tabla
+    personas = cursor.fetchall()
+    for i in personas:
+        print(i)
+
+    cursor.close()
+
+#Función para terminar la conexión con la base de datos de mysql
+#Recibe la conexión inicial con la base de datos de mysql
+def mysql_end_connection(conexion):    
+    conexion.close()
 
 
 #Función para obtener la fecha y la horas de la modificación de un objeto en un bucket 
@@ -253,7 +290,46 @@ def search_faces(image):
                                         FaceMatchThreshold=threshold,
                                          MaxFaces=maxFaces)
                 
-        faceMatches = response['FaceMatches']            
+        faceMatches = response['FaceMatches']     
+
+        
+
+        if not faceMatches:
+
+            conexion = mysql_start_connection("analitica","analitica123" , 
+            "analitica-ml.cwklrzbxbt5x.us-east-1.rds.amazonaws.com", "reconocimiento", 3306)
+
+            imagen = base64.b64encode(image)
+            #Establecer conexión con mysql
+            cursorUpdate = conexion.cursor()
+    
+            #query para actualización de la tabla de unknown
+            query = """ UPDATE unknown set imagen = %s where persona = %s"""
+
+            try:
+                #Se ejecuta la actualización
+                cursorUpdate.execute(query, ( imagen,"PersonaDesconocida")) #Se realiza la actualización
+        
+            except Exception as msg:
+
+                print(f"Oops, no se pudo actualizar el item: {msg}")
+
+            #Se termina la actualización
+            conexion.commit()
+            cursorUpdate.close()
+            
+
+            mysql_end_connection(conexion)
+
+
+
+            lambda_client = boto3.client('lambda')
+            lambda_payload = {"nombre":"nombre"}
+            lambda_payload = json.dumps(lambda_payload)
+            lambda_client.invoke(FunctionName='lambdaSNS', 
+            InvocationType='Event',
+            Payload=lambda_payload)
+
         
         for match in faceMatches:
             print('--------------------\n')
@@ -269,12 +345,14 @@ def search_faces(image):
                 similarity.append(match['Similarity'])
                 confidence.append(match['Face']['Confidence'])
                     
-        
+
+
         #Retorna lista con el ExternalImageId de las coincidencias en la colección
         #lista con las similaridades y con las confianzas de las coincidencias para cada imagen de la colección
         return listface, similarity, confidence
         
     except:
+
         #Retorna listas vacías
         return listface, similarity, confidence
 
@@ -305,40 +383,6 @@ def updateItemDB(imgId,date,time,conexion,similarity,confidence):
     conexion.commit()
     cursorUpdate.close()
 
-#Función para conectar con una base de datos de mysql
-#Recibe como parámetros el usuario, la contraseña, el host, el nombre de la base de datos
-#y el puerto de la base de datos de mysql
-def mysql_start_connection(user, password, host, database, port):
-    try:
-        #Se establece la conexión ingresando los parámetros de la base de datos de mysql
-        conexion = mysql.connector.connect(user = user, password=password, host = host, database = database, port =port)
-        print("conexion exitosa")
-    except:
-        print("falla en la conexion ")
-    #Retorna la conexión establecida con la base de datos
-    return conexion
-
-
-
-#Función para desplegar la tabla en terminal
-#Recibe la conexión inicial con la base de datos de mysql
-def mysql_display(conexion):
-
-    #Se seleccionan todos los items de la tabla
-    cursor = conexion.cursor()
-    cursor.execute("Select * from control;")
-
-    #Se crea una lista con las filas de la tabla
-    personas = cursor.fetchall()
-    for i in personas:
-        print(i)
-
-    cursor.close()
-
-#Función para terminar la conexión con la base de datos de mysql
-#Recibe la conexión inicial con la base de datos de mysql
-def mysql_end_connection(conexion):    
-    conexion.close()
 
 #Función para eliminar objetos del bucket
 #La función recibe como parámetros el nombre del bucket y de la imagen
@@ -370,7 +414,7 @@ def createRegister(conexion,imgsid,date,time):
 #Función  para obtener la diferencia de tiempo entre dos captura de una misma persona
 #La función recibe como parámetros la conexión con la base de datos de mysql, el tiempo de la captura actual
 #y el nombre de la persona detectada en la captura
-def timeDifference(conexion, endTime,nombre):
+def timeDifference(conexion, endTime,nombre,fechaActual):
     
     try:
         #Se selecciona de la tabla de registro los registros de la persona seleccionada
@@ -386,13 +430,15 @@ def timeDifference(conexion, endTime,nombre):
         numberRow = cursor.rowcount
      
         #Se obtiene la hora del  último registro de la persona
-        startTime = records[numberRow-1]
+        fecha = records[numberRow-1][0]
+        startTime = records[numberRow-1][1]
+
+        if fecha != fechaActual:
+            return 6
 
         #Se convierte la hora de string a formate datetime
         t1 = dt.strptime(startTime[0], "%H:%M:%S")
         t2 = dt.strptime(endTime, "%H:%M:%S")
-        print(t1)
-        print(t2)
 
         #Se obtiene la diferencia de los dos tiempos
         delta = t2 - t1
